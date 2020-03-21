c07b5a68428ba597b0cbd80dccb00df7a6f43992
Patrick Armentrout <patrick.armentrout@gmail.com>
2020-03-21 16:30:49 -0600

    Get form submit working

--- script.js
+++ script.js
@@ -1,14 +1,40 @@
 $(() => {
-    var element = document.querySelector("#door1");
-    element.addEventListener("click", toggleDoor);
+    let door1 = document.querySelector("#door1");
+    //door1.addEventListener("click", toggleDoor);
 
-    var door2 = document.querySelector("#door2");
-    door2.addEventListener("click", toggleDoor);
+    let door2 = document.querySelector("#door2");
+   // door2.addEventListener("click", toggleDoor);
 
-    var door3 = document.querySelector("#door3");
+    let door3 = document.querySelector("#door3");
     door3.addEventListener("click", toggleDoor);
     
-    function toggleDoor() {
+    function toggleDoor(element) {
       element.classList.toggle("doorOpen");
     }
+
+    function openDoor(door) {
+      console.log(door);
+      //toggleDoor(document.querySelector("#door1"));
+    }
+
+    $('#door1-form').click((event) => {
+      event.preventDefault();
+      let password = $('#door1-code').first().val().toLowerCase();
+
+      console.log(password);
+      
+      if (password === "test") {
+        toggleDoor(document.querySelector("#door1"));
+      }
+    });
+
+    $('#door2-form').submit((event) => {
+      event.preventDefault();
+      let password = $('#door2-form').first().val().toLowerCase();
+
+      if (password === "test") {
+        toggleDoor(document.querySelector("#door2"));
+      }
+      console.log(password);
+    });
 })
